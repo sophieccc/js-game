31cdea7fe31b9f6ee2024045571a28db7b150020
Sophie Crowley <crowleso@tcd.ie>
2020-04-18 14:12:39 +0100

    Some refactoring

--- input.js
+++ input.js
@@ -1,43 +1,26 @@
 var words = [{
         english: "book",
-        french_options: {
-            opt_one: "livre",
-            opt_two: "ouvrage",
-            opt_three: "cahier"
-        },
-        answer: 1
+        french_options: ["livre", "ouvrage", "cahier"],
+        answer: "livre"
     },
     {
         english: "house",
-        french_options: {
-            opt_one: "chambre",
-            opt_two: "habitation",
-            opt_three: "maison"
-        },
-        answer: 3
-    }, {
+        french_options: ["chambre", "habitation", "maison"],
+        answer: "maison"
+    },
+    {
         english: "university",
-        french_options: {
-            opt_one: "université",
-            opt_two: "collège",
-            opt_three: "lycée"
-        },
-        answer: 1
-    }, {
+        french_options: ["université", "collège", "lycée"],
+        answer: "université"
+    },
+    {
         english: "teacher",
-        french_options: {
-            opt_one: "étudiant",
-            opt_two: "enseignant",
-            opt_three: "apprentissage"
-        },
-        answer: 2
-    }, {
+        french_options: ["étudiant", "enseignant", "apprentissage"],
+        answer: "enseignant"
+    },
+    {
         english: "happy",
-        french_options: {
-            opt_one: "triste",
-            opt_two: "heureux",
-            opt_three: "fière"
-        },
-        answer: 2
+        french_options: ["triste", "heureux", "fière"],
+        answer: "heureux"
     }
 ];
