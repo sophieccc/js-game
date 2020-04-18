ddb089460ebe2d1278133a6ea03757ccf07177f7
Sophie Crowley <crowleso@tcd.ie>
2020-04-18 15:03:59 +0100

    Added more styling

--- index.js
+++ index.js
@@ -31,13 +31,13 @@ function Answer(guess) {
     var response = ""
     var answer = words[currentQuestion].answer
     if (words[currentQuestion].french_options[guess] == answer) {
-        response = "<p class='h4'> Correct! The answer was indeed "
+        response = "<p class='h4'> Correct!"
         score++
     } else {
-        response = "<p class='h4'> Sorry, you were incorrect. The answer was "
+        response = "<p class='h4'> Incorrect, sorry :( ."
     }
     currentQuestion++
-    response += answer + ".</p><p class='h4'>" + "Your score is currently " + score + ".</p>"
+    response += " The answer was " + answer + ".</p><p class='h4'>" + "Your score is currently " + score + ".</p>"
     document.getElementById('game').innerHTML = response
     document.getElementById("next").style.display = "inline-block";
 }
