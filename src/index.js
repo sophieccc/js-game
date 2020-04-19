5925e524df527b36eb446c5bb191b5fc8b73fb79
Sophie Crowley <crowleso@tcd.ie>
2020-04-19 19:47:43 +0100

    Randomising asked questions

--- src/index.js
+++ src/index.js
@@ -7,24 +7,39 @@ function Start() {
     score = 0;
     var result = document.getElementById("difficulty");
     var difficulty = result.options[result.selectedIndex].text;
-    processTextFile(difficulty);
+    var allWords = processTextFile(difficulty);
+    getRandomisedWords(allWords);
     var introduction = document.getElementById("start");
     introduction.style.display = "none";
     Question();
 }
 
-function processTextFile(difficulty) {
+function getRandomisedWords(allWords) {
     words = [];
+    numbers = []
+    while (numbers.length < 5) {
+        var rand = Math.floor(Math.random() * 9) + 1;
+        if (numbers.indexOf(rand) === -1) {
+            numbers.push(rand);
+        }
+    }
+    for (var i = 0; i < 5; i++) {
+        words.push(allWords[numbers[i]]);
+    }
+}
+
+function processTextFile(difficulty) {
+    var allWords = [];
     var file = new XMLHttpRequest();
     file.open("GET", "src/input.txt", false);
     file.overrideMimeType('text/plain; charset=UTF-8');
     file.onreadystatechange = function() {
         if (file.readyState === 4) {
             var lines = file.responseText.split('\n');
-            for (var i = 0; i < lines.length - 1; i++) {
+            for (var i = 0; i < lines.length; i++) {
                 if (lines[i].startsWith(difficulty)) {
                     var outputs = lines[i].split(',');
-                    words.push({
+                    allWords.push({
                         english: outputs[1],
                         french_options: [outputs[3], outputs[4], outputs[5]],
                         answer: outputs[2]
@@ -34,6 +49,7 @@ function processTextFile(difficulty) {
         }
     }
     file.send();
+    return allWords;
 }
 
 function Question() {
