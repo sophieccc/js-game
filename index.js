78349ef6653e1a7223ac404173dc031214faf6da
Sophie Crowley <crowleso@tcd.ie>
2020-04-19 03:29:25 +0100

    Changed to text input method

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 var currentQuestion = 0;
 var score = 0;
+var words = [];
 
 function Start() {
     var startButton = document.getElementById("start");
     startButton.parentNode.removeChild(startButton);
-
-    var statement = document.createElement("p");
-    var optionOne = document.createElement("button");
-    var optionTwo = document.createElement("button");
-    var optionThree = document.createElement("button");
-
-    optionOne.classList = "btn btn-primary";
-    optionTwo.classList = "btn btn-primary";
-    optionThree.classList = "btn btn-primary";
-    optionOne.setAttribute("onclick", "Answer(0)");
-    optionTwo.setAttribute("onclick", "Answer(1)");
-    optionThree.setAttribute("onclick", "Answer(2)");
-    optionOne.id = "one";
-    optionTwo.id = "two";
-    optionThree.id = "three";
-    statement.id = "statement";
-
-    var parent = document.getElementById("game");
-    parent.appendChild(statement)
-    parent.appendChild(optionOne);
-    parent.appendChild(optionTwo);
-    parent.appendChild(optionThree);
-
+    processTextFile();
     Question();
 }
 
@@ -39,33 +18,58 @@ function Replay() {
 
 function Question() {
     document.getElementById("next").style.display = "none";
+
     var statement = document.getElementById("statement");
     if (currentQuestion < words.length) {
-        statement.textContent = "Guess the french word for " + words[currentQuestion].english + " .";
+        statement.textContent = "Guess the french word for " + words[currentQuestion].english + ".";
+        for (let element of document.getElementsByClassName("option")) {
+            element.style.display = "inline-block";
+        }
         document.getElementById("one").innerText = words[currentQuestion].french_options[0];
         document.getElementById("two").innerText = words[currentQuestion].french_options[1];
         document.getElementById("three").innerText = words[currentQuestion].french_options[2];
     } else {
-        var result = document.createElement("p");
-        statement.textContent = "You finished the game! You got a total score of " + score +
-            " out of " + currentQuestion + ".";
-
+        statement.textContent = "You finished the game! You got a total score of " +
+            score + " out of " + currentQuestion + ".";
         document.getElementById("replay").style.display = "inline-block";
     }
 }
 
 function Answer(guess) {
-    var response
+    for (let element of document.getElementsByClassName("option")) {
+        element.style.display = "none";
+    }
     var answer = words[currentQuestion].answer;
+    var resultMessage;
+    var statement = document.getElementById("statement");
     if (words[currentQuestion].french_options[guess] == answer) {
-        response = "<p class='h4'> Correct!";
+        resultMessage = "Correct!";
         score++;
     } else {
-        response = "<p class='h4'> Incorrect, sorry :( .";
+        resultMessage = "Incorrect, sorry :( .";
     }
     currentQuestion++;
-    response += " The answer was " + answer + ".</p><p class='h4'>" + "Your score is currently " +
-        score + ".</p>";
-    document.getElementById('game').innerHTML = response;
+    statement.textContent = resultMessage + " The answer was " + answer + ".";
     document.getElementById("next").style.display = "inline-block";
+}
+
+function processTextFile() {
+    var file = new XMLHttpRequest();
+    file.open("GET", "input.txt", false);
+    file.overrideMimeType('text/plain; charset=x-user-defined');
+    file.onreadystatechange = function() {
+        if (file.readyState === 4) {
+            console.log(file.responseText);
+            var lines = file.responseText.split('\n');
+            for (var i = 0; i < lines.length - 1; i++) {
+                var outputs = lines[i].split(',');
+                words.push({
+                    english: outputs[0],
+                    french_options: [outputs[2], outputs[3], outputs[4]],
+                    answer: outputs[1]
+                })
+            }
+        }
+    }
+    file.send();
 }
